5d3ddb9ff1f9de66c6ccf5c324eefd8b4dbc2f71
Youssef-Bouayez <youssefbouyez2@gmail.com>
2023-05-10 20:22:10 +0300

    creat database and validation in symfony

---
+++ src/Entity/Ingredient.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\IngredientRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+#[ORM\Entity(repositoryClass: IngredientRepository::class)]
+class Ingredient
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    #[Assert\NotBlank]
+    #[Assert\Length( min: 2,max: 50)]
+    private ?string $nom = null;
+
+    #[ORM\Column]
+    #[Assert\NotNULL]
+    #[Assert\Positive]
+    #[Assert\LessThan(200)]
+    private ?float $prix = null;
+
+    #[ORM\Column]
+    #[Assert\NotNULL]
+    private ?\DateTimeImmutable $creatAt = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrix(): ?float
+    {
+        return $this->prix;
+    }
+
+    public function setPrix(float $prix): self
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+
+    public function getCreatAt(): ?\DateTimeImmutable
+    {
+        return $this->creatAt;
+    }
+
+    public function setCreatAt(\DateTimeImmutable $creatAt): self
+    {
+        $this->creatAt = $creatAt;
+
+        return $this;
+    }
+}
