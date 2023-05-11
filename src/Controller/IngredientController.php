866b792930e2b6e9dd16bea0d870a517c80a9ae0
Youssef-Bouayez <youssefbouyez2@gmail.com>
2023-05-12 00:00:22 +0300

    modifier sur formulaire et afficher validation

--- src/Controller/IngredientController.php
+++ src/Controller/IngredientController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+
+use App\Entity\Ingredient;
+use App\Form\IngredientType;
 use App\Repository\IngredientRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -34,4 +38,29 @@ class IngredientController extends AbstractController
             'pagination' => $ingredients
         ]);
     }
+    #[Route('/ingredient/nouveau', name: 'new_ingredient', methods:['GET' , 'POST'])]
+    public function new(
+        Request $request ,
+        EntityManagerInterface $manager,
+        ) :Response
+    {
+        $ingredient = new Ingredient();
+        $form = $this->createForm(IngredientType::class, $ingredient) ;
+
+         $form->handleRequest($request);
+        // $form->submit($request->request->get($form->getName()));
+        if ($form->isSubmitted() && $form->isValid()) {
+            $ingredient = $form->getData();
+            $manager->persist($ingredient);
+            $manager->flush();
+            
+        }
+        
+
+        return $this->render('pages/ingredient/new.html.twig' , [
+            'form' => $form->createView()
+        ]);
+    }
+
+    
 }
